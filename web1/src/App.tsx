57e11ead4271dada7d1c0758a14977d9f990ce19
Janderson Fonseca <janderson-fonseca@hotmail.com>
2020-06-04 23:19:55 -0300

    inicio do projeto web

---
+++ web1/src/App.tsx
@@ -0,0 +1,23 @@
+import React, {useState} from 'react';
+import './App.css';
+import Header from './Header';
+
+function App() {
+  const [counter, setCounter] = useState(0);
+
+  function handleButtonClick(){
+    setCounter(counter + 1);
+  }
+
+  return (        
+        <div>
+          <Header title="Hello World"/>
+          <h1>
+            {counter}
+          </h1>
+          <button type="button" onClick={handleButtonClick}>Aumentar</button>
+        </div>
+  );
+}
+
+export default App;
